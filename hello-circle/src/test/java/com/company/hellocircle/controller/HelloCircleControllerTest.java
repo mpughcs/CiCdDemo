d4a7b03d34cdd7757028616ccfff9e2682f8f68a
Max Pugh <maxpugh@Maxs-MBP.lan>
2023-08-17 12:26:54 -0700

    adding correct test code

--- hello-circle/src/test/java/com/company/hellocircle/controller/HelloCircleControllerTest.java
+++ hello-circle/src/test/java/com/company/hellocircle/controller/HelloCircleControllerTest.java
@@ -1,14 +1,30 @@
 package com.company.hellocircle.controller;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(HelloCircleController.class)
 public class HelloCircleControllerTest {
-    public MockMvc mockMvc;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnHello() throws Exception {
+        // arrange and act
+        mockMvc.perform(get("/hello"))
+                .andDo(print())
+                .andExpect(status().isOk())
+
+                // assert
+                .andExpect(content().string("Hello, Circle!"));
+    }
 
     @Test
     public void shouldReturnAnswer() throws Exception {
@@ -16,6 +32,7 @@ public class HelloCircleControllerTest {
         mockMvc.perform(get("/answer"))
                 .andDo(print())
                 .andExpect(status().isOk())
+
                 // assert
                 .andExpect(content().string("The answer is 42!"));
     }
